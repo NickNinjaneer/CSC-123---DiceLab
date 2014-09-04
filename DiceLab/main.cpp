dda8a9cf93459514462fb1d73ae63072a91b64c4
Nick Williams <Ninjaneer@Nicks-MacBook-Pro.local>
2014-09-04 08:00:05 -0500

    Initial Commit

---
+++ DiceLab/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DiceLab
+//
+//  Created by Nick Williams on 9/4/14.
+//  Copyright (c) 2014 NinjaneerM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
